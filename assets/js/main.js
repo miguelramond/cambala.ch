09bd09212a9d3deb6a2bb4c7da6518b5b87314d6
miguelramond <90074066+miguelramond@users.noreply.github.com>
2021-11-16 22:04:28 -0300

    Color changer - randomized

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,15 @@
-if($("#home").length > 0) {
-    alert("¡Bienvenidos a mi sitio!");
- }
+function colorize() {
+    // Create variable with hexadecimal characters for randomizing
+    var letters = "0123456789ABCDEF".split('');
+
+    // Initialize color variable
+    var color = "#";
+
+    // For loop generating hexadecimal code out of randomized results from letters array
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+
+    // Add generated color to page
+    document.body.style.backgroundColor = color;
+}
